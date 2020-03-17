1d6f42c95bcdeeac730e7be45926449e1715f435
miluku1018 <miluku1018@gmail.com>
2020-03-17 19:37:47 +0800

    update 0_select_basics

--- 0 SELECT basics.sql
+++ 0 SELECT basics.sql
@@ -15,7 +15,6 @@ SELECT name, area FROM world
 -- Bahrain	1234571
 -- Swaziland	1220000
 -- Timor-Leste	1066409
-
 SELECT name, population FROM world
  WHERE population BETWEEN 1000000 AND 1250000;
 
